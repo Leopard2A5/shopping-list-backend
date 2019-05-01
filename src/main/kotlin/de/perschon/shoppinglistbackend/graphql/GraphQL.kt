4494bc350b172e7b991c12e5d91fea46d672efdf
René Perschon <rperschon85@gmail.com>
2019-05-01 21:11:59 +0200

    move schema def into files

--- src/main/kotlin/de/perschon/shoppinglistbackend/graphql/GraphQL.kt
+++ src/main/kotlin/de/perschon/shoppinglistbackend/graphql/GraphQL.kt
@@ -1,6 +1,5 @@
 package de.perschon.shoppinglistbackend.graphql
 
-import de.perschon.shoppinglistbackend.products.ProductService
 import graphql.GraphQL
 import graphql.schema.idl.RuntimeWiring.newRuntimeWiring
 import graphql.schema.idl.SchemaGenerator
@@ -8,26 +7,22 @@ import graphql.schema.idl.SchemaParser
 import graphql.schema.idl.TypeRuntimeWiring
 import kotlinx.coroutines.runBlocking
 import org.koin.standalone.StandAloneContext
+import java.io.File
+import java.io.InputStreamReader
 import java.util.function.UnaryOperator
 import kotlin.coroutines.suspendCoroutine
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.declaredFunctions
 
 fun buildGraphQL(): GraphQL {
-    val productService = StandAloneContext.getKoin().koinContext.get<ProductService>()
     val query = StandAloneContext.getKoin().koinContext.get<Query>()
 
-    val schemaString = """
-        type Query {
-            products: [Product!]!
-            product(id: String!): Product
-        }
-
-        type Product {
-            id: String!
-            name: String!
-        }
-    """.trimIndent()
+    val schemaString = File(object{}.javaClass.classLoader.getResource("schema").toURI()).let { file ->
+        file.list()
+            .map { File(file, it) }
+            .map { it.inputStream().reader().use(InputStreamReader::readText) }
+            .joinToString(separator = "\n")
+    }
 
     val schemaParser = SchemaParser()
     val typeDefinitionRegistry = schemaParser.parse(schemaString)
